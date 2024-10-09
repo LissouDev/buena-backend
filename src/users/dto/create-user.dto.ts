91df1626044092107a34143ac201a17c23d48103
Alice Grandjean <alice.grandj.49@gmail.com>
2024-10-09 11:56:42 +0200

    added validation on user dto

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,6 +1,25 @@
+import {
+  IsEmail,
+  IsEnum,
+  IsString,
+  IsNotEmpty,
+  IsMobilePhone,
+} from 'class-validator';
+
 export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
   name: string;
+
+  @IsEmail()
   email: string;
+
+  @IsString()
+  @IsMobilePhone()
   phone: string;
+
+  @IsEnum(['0-1000', '1000-2000', '2000-3000', '3000-4000', '4000+'], {
+    message: 'Salary range required',
+  })
   salary: '0-1000' | '1000-2000' | '2000-3000' | '3000-4000' | '4000+';
 }
